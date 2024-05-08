434ffa71260495100458aa0a51863c2bd050f12f
Mohd Belal Naim <contactbelalnaim@gamil.com>
2024-05-08 12:50:39 +0530

    Trasferred firebnase access to insha hasn

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyDHz6VtdKlZ-mJwyU0YpO3WDkSCHKXYTV8",
-  authDomain: "shield-65adc.firebaseapp.com",
-  projectId: "shield-65adc",
-  storageBucket: "shield-65adc.appspot.com",
-  messagingSenderId: "376787900169",
-  appId: "1:376787900169:web:dba0f705216bb61b3da5d8",
+  apiKey: "AIzaSyDC8ebiIOUlqb6JiAppKxHvZNJYAfAbx94",
+  authDomain: "shield-app-b528f.firebaseapp.com",
+  projectId: "shield-app-b528f",
+  storageBucket: "shield-app-b528f.appspot.com",
+  messagingSenderId: "1083588258840",
+  appId: "1:1083588258840:web:af99a3a1c72c3f6325276d",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
